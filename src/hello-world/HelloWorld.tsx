33304e5c1cf65161c9741f28f0357c7dc68af214
Angga Yuda <anggayudap@outlook.com>
2026-01-08 21:33:53 +0700

    feature: split component w/ function

--- src/hello-world/HelloWorld.tsx
+++ src/hello-world/HelloWorld.tsx
@@ -1,9 +1,21 @@
 export default function HelloWorld() {
     return (
         <div>
-            <h1>Hello, World!</h1>
-            <p>Welcome to my React application.</p>
+            <HelloWorldHeader />
+            <HelloWorldParagraph />
         </div>
     )
-    
-    }
+
+}
+
+function HelloWorldHeader() {
+    return (
+        <h1>Hello, World!</h1>
+    )
+}
+
+function HelloWorldParagraph() {
+    return (
+        <p>Welcome to my React application. Split function!</p>
+    )
+}
